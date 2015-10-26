afdb73f30b3acc58c551c97209d155eea46c10cc
Chris Maddalena <chris.maddalena@gmail.com>
2015-10-25 22:05:42 -0400

    Added support for CIDR
    Importing re and netaddr to use regex (identifying CIDR addresses) and
    enumerating the IP addresses in the range for lookups.

--- rev.py
+++ rev.py
@@ -4,6 +4,8 @@
 # Goes through a list of ip addresses and tries to do a reverse lookup
 import sys
 import dns.resolver,dns.reversename
+import re
+from netaddr import *
 from time import sleep
 
 if len(sys.argv) < 3:
@@ -24,13 +26,26 @@ resolver = dns.resolver.Resolver()
 resolver.namerservers = [dnsserver]
 for i in ipfile:
     try:
-        i = i.rstrip()
-        # Format the ip in reverse
-        ip = '.'.join(reversed(i.split("."))) + ".in-addr.arpa"
-        answer = resolver.query(ip, "PTR")
-        for a in answer:
-            print i, " - ",a
-        # Sleep for specified amount of time
-        sleep(delay)
+        # Match if the IP address is a range using CIDR
+        if re.match('^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$',i):
+            for ip in IPNetwork(i):
+                ip = str(ip).rstrip()
+                # Format the ip in reverse
+                ipaddr = '.'.join(reversed(ip.split("."))) + ".in-addr.arpa"
+                answer = resolver.query(ipaddr, "PTR")
+                for a in answer:
+                    print ip, " - ",a
+                # Sleep for specified amount of time
+                sleep(delay)
+        # If it's not a CIDR range, proceed with resolving
+        else:
+            i = i.rstrip()
+            # Format the ip in reverse
+            ip = '.'.join(reversed(i.split("."))) + ".in-addr.arpa"
+            answer = resolver.query(ip, "PTR")
+            for a in answer:
+                print i, " - ",a
+            # Sleep for specified amount of time
+            sleep(delay)
     except:
         continue
